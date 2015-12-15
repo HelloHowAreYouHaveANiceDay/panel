e180c20babf3a9665e83a6e62b7cdfb03d85a723
HelloHowAreYouHaveANiceDay <liboli.lilibo@gmail.com>
2015-12-14 23:48:26 -0500

    Trying new things

--- client/templates/forecast.js
+++ client/templates/forecast.js
@@ -1,5 +1,10 @@
 Template.forecast.helpers({
-	 forecast : function(){
-		return Forecasts.findOne();}
+	forecast : function(){
+		return Forecasts.findOne();},
+	getForecastDate : function(d){
+		var date = new Date(d);
+		return date;
+	}
+	
 		
 });
